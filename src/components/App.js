b968f3efac351dc80453c23f1c510efc62dadc06
ArtRosh <artem.rosh17@gmail.com>
2025-05-23 14:49:40 -0700

    set up all components

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,10 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
+
   const appClass = storeValue ? "App dark" : "App light"
 
-  const backgroundColor = storeValue ? "Dark Mode" : "Light Mode"
+  const textButton = storeValue ? "Dark Mode" : "Light Mode"
 
   
 
@@ -18,7 +19,7 @@ function App() {
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button onClick={() => finalValue(value => !value)}>{backgroundColor}</button>
+        <button onClick={() => finalValue(value => !value)}>{textButton}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [value, setUp] = useState(false)
+
+  const ifInCard = value ? "in-cart" : ""
+
+  
+  function ifAddedToCard () {
+    return setUp(ifInCard => !ifInCard)
+  }
+
+  function textButton () {
+    function i () {
+    return value ? "Remove From Cart" : "Add to Cart"
+    }
+    return i ()
+  }
+
+
   return (
-    <li className="">
+    <li className={ifInCard}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add"
+      
+      onClick={ifAddedToCard}
+      
+    >{textButton ()}</button>
     </li>
   );
 }
