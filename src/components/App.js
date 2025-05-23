39ae4e58bdb4793d6811cc18e1ab592c2f37be56
ArtRosh <artem.rosh17@gmail.com>
2025-05-23 09:43:04 -0700

    set up background color

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState }from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
+  const [storeValue, finalValue] = useState(false)
+
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = storeValue ? "App dark" : "App light"
+
+  const backgroundColor = storeValue ? "Dark Mode" : "Light Mode"
+
+  
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={() => finalValue(value => !value)}>{backgroundColor}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
